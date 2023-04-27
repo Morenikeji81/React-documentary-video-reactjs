196b3e0a1e58375a14e52209d965206d9ea600a6
Adewale Oluwatoyin <adewaleoluwatoyin81@gmail.com>
2023-04-27 03:10:35 +0100

    A React Documentary Video

---
+++ src/components/Search.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+export default function Search() {
+   
+  return (
+    <div > 
+      
+    <h1 className="documentary-container-header">React Documentary Video </h1>
+    
+    <div className="documentary--container">
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/8pDqJVdNa44" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    <div className='video--desription'>
+    <h3><a href="https://www.youtube.com/embed/8pDqJVdNa44">React: The Documentary</a></h3> 
+    <p>The origin story of React</p>
+    
+    </div>
+
+    <div>
+      
+    </div>
+    </div>
+
+    <div className="documentary--container">
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/WzRSysq7F4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen> 
+    </iframe>
+    <div className='video--desription'>
+    <h3><a href="https://www.youtube.com/embed/WzRSysq7F4k">React: Question and Answer</a></h3> 
+    <p>Question and Answer with the cast of React.js</p>
+    </div>
+    <div>
+      
+    </div>
+    </div>
+
+    <div className="documentary--container">
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/x7cQ3mrcKaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    <div className='video--desription'>
+    <h3><a href="https://www.youtube.com/embed/x7cQ3mrcKaY">Rethinking Best Practices</a></h3> 
+    <p>Pete Hunt (2013)</p>
+    </div>
+    <div>
+     
+    </div>
+    </div>
+   
+    <div className="documentary--container">
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/V-QO-KO90iQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    <div className='video--desription'>
+    <h3><a href="https://www.youtube.com/embed/V-QO-KO90iQ">Introducing React Hooks</a></h3> 
+    <p>Sophie Alphert and Dan Abramov (2018)</p>
+    </div>
+    </div>
+
+    <div className="documentary--container" >
+    <iframe width="360" height="315" src="https://www.youtube.com/embed/TQQPAU21ZUw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    <div className='video--desription'>
+    <h3><a href="https://www.youtube.com/embed/V-QO-KO90iQ">Introducing Server Components</a></h3> 
+    <p>Dan Abramov and Lauren Tan (2020)</p>
+    </div>
+    <div>
+     
+    </div>
+    </div>
+
+  </div>
+
+  )
+}
+
+
+
+
